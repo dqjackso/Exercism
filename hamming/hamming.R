32a1a18d7c07bc0c8eff30da8cfb6e2b686a9570
Derek <derek.q.jackson@gmail.com>
2025-05-05 17:04:11 -0700

    hamming solution

--- hamming/hamming.R
+++ hamming/hamming.R
@@ -1,9 +1,24 @@
 # This is a stub function to take two strings
 # and calculate the hamming distance
+
 hamming <- function(strand1, strand2) {
-  if (nchar(strand1) != nchar(strand2)) {
-    stop
+  if (nchar(strand2) != nchar(strand1)) {
+    stop()
+  } else if (nchar(strand1) == 0 || nchar(strand2) == 0) {
+    0
   } else {
-    as.number(nchar(Reduce(setdiff, strsplit(c(strand1, strand2), split = ""))))
+    first <- strsplit(strand1, split = "")
+    
+    second <- strsplit(strand2, split = "")
+    
+    count <- 0
+    
+    for (i in 1:nchar(strand1)) {
+      if (first[[1]][i] != second[[1]][i]) {
+        count <- count + 1
+      }
+    }
+    
+    count
   }
 }
